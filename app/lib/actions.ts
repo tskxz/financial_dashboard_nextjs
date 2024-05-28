5adb8af5f7b545196103d619e23a89a2c518af69
Tanjil <tanjilkh@gmail.com>
2024-05-28 11:14:57 +0100

    auth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,8 @@ import {z} from 'zod'
 import {sql} from '@vercel/postgres'
 import {revalidatePath} from 'next/cache'
 import {redirect} from 'next/navigation'
+import {signIn} from '@/auth'
+import {AuthError} from 'next-auth'
 
 const FormSchema = z.object({
     id: z.string(),
@@ -52,4 +54,23 @@ export async function updateInvoice(id: string, formData: FormData) {
 export async function deleteInvoice(id: string) {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try{
+    await signIn('credentials', formData)
+  } catch (err){
+    if(err instanceof AuthError){
+      switch(err.type){
+        case 'CredentialsSignin':
+          return 'Invalid credentials.'
+        default:
+          return 'Something went wrong'
+      }
+    }
+    throw err;
+  }
 }
